853f8d9b0ea20a8ccd4214b31053bb7c92afbdd7
Juraj Antas <jantas@sygic.com>
2020-05-18 09:44:32 +0200

    Initial Commit

---
+++ dataConverter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dataConverter
+//
+//  Created by Juraj Antas on 18/05/2020.
+//  Copyright © 2020 Sygic a.s. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
